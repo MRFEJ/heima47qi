b0deffa5b064594690a3a0c5038ce50ff302a267
MRFEJ <1403676630@qq.comgit config --global user.name Your>
2020-02-20 18:19:06 +0800

    完成登录框的基本样式和布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,12 +1,110 @@
 <template>
   <div class="login">
-    <div class="log-left"></div>
-    <img src="./img/login_banner_ele.png" alt="">
+    <div class="log-left">
+      <div class="title-box">
+        <img class="box-img" src="./img/logo.png" alt />
+        <span class="box-s">黑马面面</span>
+        <span class="box-f"></span>
+        <span class="box-s">用户登录</span>
+      </div>
+      <el-form ref="form" :model="form" label-width="0px" :rules="rules">
+        <el-form-item prop="user">
+          <el-input placeholder="请输入手机号" clearable v-model="form.user" prefix-icon="el-icon-user"></el-input>
+        </el-form-item>
+
+        <el-form-item prop="password">
+          <el-input
+            placeholder="请输入密码"
+            clearable
+            show-password
+            v-model="form.password"
+            prefix-icon="el-icon-lock"
+          ></el-input>
+        </el-form-item>
+
+        <el-form-item prop="code">
+          <el-row>
+            <el-col :span="18">
+              <el-input
+                placeholder="请输入验证码"
+                clearable
+                v-model="form.code"
+                prefix-icon="el-icon-key"
+              ></el-input>
+            </el-col>
+            <el-col :span="6">
+              <img class="yz" src="./img/code.png" alt />
+            </el-col>
+          </el-row>
+        </el-form-item>
+
+        <el-form-item prop="type">
+          <el-checkbox v-model="form.type">
+            我已阅读并同意
+            <el-link type="primary">用户协议</el-link>和
+            <el-link type="primary">隐私条款</el-link>
+          </el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-row>
+            <el-col>
+              <el-button style="width:100%" type="primary" @click="submitForm">登陆</el-button>
+              <el-button style="width:100%" type="primary" @click="submitForm">注册</el-button>
+            </el-col>
+          </el-row>
+        </el-form-item>
+      </el-form>
+    </div>
+    <img src="./img/login_banner_ele.png" alt />
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      form: {
+        user: "",
+        password: "",
+        code: "",
+        type: []
+      },
+      rules: {
+        user: [
+          { required: true, message: "手机号不能为空", trigger: "blur" },
+          { min: 11, max: 11, message: "请输入正确的手机号", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "密码不能为空", trigger: "blur" },
+          { min: 6, max: 12, message: "请输入正确的密码", trigger: "blur" }
+        ],
+        code: [
+          { required: true, message: "验证码不能为空", trigger: "blur" },
+          { min: 3, max: 5, message: "请输入验证码", trigger: "blur" }
+        ],
+        type: [
+          {
+            required: true,
+            message: "请勾选我已阅读并同意用户协议和隐私条款",
+            trigger: "change"
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm() {
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    }
+  }
+};
 </script>
 
 <style lang="less">
@@ -22,10 +120,50 @@ export default {};
   justify-content: space-around;
   align-items: center;
   .log-left {
-  width: 478px;
-  height: 550px;
-  background: rgba(245, 245, 245, 1);
-}
-
+    width: 478px;
+    height: 550px;
+    background: rgba(245, 245, 245, 1);
+    padding: 48px;
+    .title-box {
+      display: flex;
+      align-items: center;
+      margin-bottom: 27px;
+      .box-img {
+        width: 22px;
+        height: 17px;
+        margin-right: 16px;
+      }
+      .box-f {
+        width: 1px;
+        height: 28px;
+        background: rgba(199, 199, 199, 1);
+        margin-left: 14px;
+        margin-right: 12px;
+      }
+      .box-s {
+        font-size: 24px;
+        font-weight: 400;
+        color: rgba(12, 12, 12, 1);
+      }
+    }
+    .yz {
+      width: 100%;
+      height: 40px;
+    }
+    .el-checkbox {
+      display: flex;
+      .el-checkbox__label {
+        display: flex;
+        align-items: center;
+        margin-top: -3px;
+      }
+    }
+    .el-button{
+      &:nth-child(2){
+        margin-left: 0px;
+        margin-top: 26px;
+      }
+    }
+  }
 }
 </style>
